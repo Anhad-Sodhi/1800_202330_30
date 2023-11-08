0ad955f1332d9179898a85f125bb43e1b83f4922
Dan Didac <didacdan@gmail.com>
2023-11-07 20:08:15 -0800

    Fixed money input and user id data relation

--- scripts/subdata.js
+++ scripts/subdata.js
@@ -26,6 +26,54 @@ function getNameFromAuth() {
 }
 getNameFromAuth(); //run the function
 
+//photo
+var ImageFile;
+function listenFileSelect() {
+      // listen for file selection
+      var fileInput = document.getElementById("food-photo");
+
+			// When a change happens to the File Chooser Input
+      fileInput.addEventListener('change', function (e) {
+          ImageFile = e.target.files[0];   //Global variable
+      })
+}
+listenFileSelect();
+
+function uploadPic(postDocID) {
+    console.log("inside uploadPic " + postDocID);
+    var storageRef = storage.ref("images/" + postDocID + ".jpg");
+
+    storageRef.put(ImageFile)   //global variable ImageFile
+       
+                   // AFTER .put() is done
+        .then(function () {
+            console.log('2. Uploaded to Cloud Storage.');
+            storageRef.getDownloadURL()
+
+                 // AFTER .getDownloadURL is done
+                .then(function (url) { // Get URL of the uploaded file
+                    console.log("3. Got the download URL.");
+
+                    // Now that the image is on Storage, we can go back to the
+                    // post document, and update it with an "image" field
+                    // that contains the url of where the picture is stored.
+                    db.collection("listings").doc(postDocID).update({
+                            "image": url // Save the URL into users collection
+                        })
+                         // AFTER .update is done
+                        .then(function () {
+                            console.log('4. Added pic URL to Firestore.');
+                            // One last thing to do:
+                            // save this postID into an array for the OWNER
+                            // so we can show "my posts" in the future
+                            savePostIDforUser(postDocID);
+                        })
+                })
+        })
+        .catch((error) => {
+             console.log("error uploading to cloud storage");
+        })
+}
 submit.addEventListener('click', (e) => {
     e.preventDefault();
     db.collection('listings').doc().set({
