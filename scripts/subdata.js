506360f9b721f59a2a67fe976f10562a83f19a0d
Dan Didac <didacdan@gmail.com>
2023-11-08 12:33:03 -0800

    Photo stuff

--- scripts/subdata.js
+++ scripts/subdata.js
@@ -39,6 +39,33 @@ function listenFileSelect() {
 }
 listenFileSelect();
 
+function savePost() {
+    alert ("SAVE POST is triggered");
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here. 
+            db.collection("posts").add({
+                foodName: prodName.value,
+                foodType: foodType.value,
+                foodPrice: price.value,
+                foodDescription: description.value,
+                address: address.value,
+                user: userName,
+                last_updated: firebase.firestore.FieldValue
+                    .serverTimestamp() //current system time
+            }).then(doc => {
+                console.log("1. Post document added!");
+                console.log(doc.id);
+                uploadPic(doc.id);
+            })
+        } else {
+            // No user is signed in.
+                          console.log("Error, no user signed in");
+        }
+    });
+}
+
 function uploadPic(postDocID) {
     console.log("inside uploadPic " + postDocID);
     var storageRef = storage.ref("images/" + postDocID + ".jpg");
@@ -57,7 +84,7 @@ function uploadPic(postDocID) {
                     // Now that the image is on Storage, we can go back to the
                     // post document, and update it with an "image" field
                     // that contains the url of where the picture is stored.
-                    db.collection("listings").doc(postDocID).update({
+                    db.collection("posts").doc(postDocID).update({
                             "image": url // Save the URL into users collection
                         })
                          // AFTER .update is done
@@ -74,17 +101,39 @@ function uploadPic(postDocID) {
              console.log("error uploading to cloud storage");
         })
 }
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    db.collection('listings').doc().set({
-        // photo: photo.value,
-        foodName: prodName.value,
-        foodType: foodType.value,
-        foodPrice: price.value,
-        foodDescription: description.value,
-        address: address.value,
-        user: userName,
-    }).then(() => {
-        submissionForm.reset();      
+
+//--------------------------------------------
+//saves the post ID for the user, in an array
+//--------------------------------------------
+function savePostIDforUser(postDocID) {
+    firebase.auth().onAuthStateChanged(user => {
+          console.log("user id is: " + user.uid);
+          console.log("postdoc id is: " + postDocID);
+          db.collection("users").doc(user.uid).update({
+                myposts: firebase.firestore.FieldValue.arrayUnion(postDocID)
+          })
+          .then(() =>{
+                console.log("5. Saved to user's document!");
+                                alert ("Post is complete!");
+                //window.location.href = "showposts.html";
+           })
+           .catch((error) => {
+                console.error("Error writing document: ", error);
+           });
     })
-});
+}
+
+// submit.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     db.collection('listings').doc().set({
+//         // photo: photo.value,
+//         foodName: prodName.value,
+//         foodType: foodType.value,
+//         foodPrice: price.value,
+//         foodDescription: description.value,
+//         address: address.value,
+//         user: userName,
+//     }).then(() => {
+//         submissionForm.reset();      
+//     })
+// });
