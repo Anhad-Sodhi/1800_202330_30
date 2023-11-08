9b54c8b7b7ae2ce257d75382358cb6ad837af844
Matthew Dodd <matthew_dodd_94@hotmail.com>
2023-11-08 10:55:15 -0800

    Improved listing functionality

--- scripts/listing.js
+++ scripts/listing.js
@@ -5,20 +5,24 @@ function redirectEmail() {
 // Create listings and populate them for each document in firebase
 var count = 1;
 
-const citiesRef = db.collection('listings');
-const snapshot = await citiesRef.get();
+const listingsRef = db.collection('listings');
+const snapshot = await listingsRef.get();
 snapshot.forEach(doc => {
     // Create listing
     var listing = document.createElement("div");
-    listings.setAttribute("class", "list");
+    listing.setAttribute("class", "list");
     var productName = document.createElement("p");
     productName.setAttribute("class", "prodName");
+    productName.setAttribute("id", "productName" + count);
     var userName = document.createElement("p");
     userName.setAttribute("class", "userName");
+    userName.setAttribute("id", "userName" + count);
     var price = document.createElement("p");
-    price.setAttribute("class", "Price");
+    price.setAttribute("class", "price");
+    price.setAttribute("id", "price" + count);
     var information = document.createElement("p");
     information.setAttribute("class", "info");
+    information.setAttribute("id", "information" + count);
 
     listing.appendChild(productName);
     listing.appendChild(userName);
@@ -29,10 +33,10 @@ snapshot.forEach(doc => {
 
     
     // Update info on listing
-    document.getElementById("productName" + count).innerHTML = doc.data().productName;      
-    document.getElementById("userName" + count).innerHTML = doc.data().posterName;
-    document.getElementById("information" + count).innerHTML = doc.data().description;      
-    document.getElementById("price" + count).innerHTML = "$" + doc.data().price;      
+    document.getElementById("productName" + count).innerHTML = doc.data().foodName;      
+    document.getElementById("userName" + count).innerHTML = doc.data().user;
+    document.getElementById("information" + count).innerHTML = doc.data().foodDescription;      
+    document.getElementById("price" + count).innerHTML = "$" + doc.data().foodPrice;      
 
     count++;
     console.log(count);
