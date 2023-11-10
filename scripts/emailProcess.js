7f0f2236d0d56e4eb93ac6317f1950b938a1fe6e
Anhad Sodhi <anhadsodhi2005@gmail.com>
2023-11-09 21:33:18 -0800

    Made footer icon a plus on a circle using CSS shape, rather than a full-width rectangle. Also put working copy icon beside e

--- scripts/emailProcess.js
+++ scripts/emailProcess.js
@@ -2,3 +2,9 @@ document.getElementById("email").addEventListener("click",
     function() {
         window.location.href = "mailto:" + document.getElementById("email").innerText;
 });
+
+document.getElementById("copyButton").addEventListener("click", 
+    function() {
+        let text = document.getElementById("email").innerText;
+        navigator.clipboard.writeText(text);
+});
