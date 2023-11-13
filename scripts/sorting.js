181a3d3fd83f8a77e8f2f41cd7981edfb4ee5801
Anhad Sodhi <anhadsodhi2005@gmail.com>
2023-11-13 13:48:45 -0800

    Made sorting buttons a dropdown menu on browsing page, added sort by price (high to low)

--- scripts/sorting.js
+++ scripts/sorting.js
@@ -1,6 +1,6 @@
 let allListings = document.getElementsByClassName('list');
 
-function sortByMoney() {
+function sortByPriceAscending() {
     // sort listings by lowest price
     let sortedList = Array.from(allListings).sort(function(a, b) {
         a = a.getElementsByClassName('price')[0];
@@ -58,4 +58,30 @@ async function sortByDate() {
     // clear listings on page and add listings in new sorted order
     
 
+}
+
+function sortByPriceDescending() {
+    // sort listings by lowest price
+    let sortedList = Array.from(allListings).sort(function(a, b) {
+        a = a.getElementsByClassName('price')[0];
+        b = b.getElementsByClassName('price')[0];
+
+        // get an innerHTML string for each sort item
+        let a1 = a.innerHTML;
+        let b1 = b.innerHTML;
+
+        // remove $ from each one
+        a1 = a1.replace('\$', '');
+        b1 = b1.replace('\$', '');
+
+        // compare each one
+        return parseInt(b1) - parseInt(a1);
+    })
+    console.log(sortedList);
+    
+    // clear listings on page and add listings in new sorted order
+    document.getElementById('listings').innerHTML = "";
+    for (let i = 0; i < sortedList.length; i++) {
+        document.getElementById('listings').appendChild(sortedList[i]);
+    }
 }
